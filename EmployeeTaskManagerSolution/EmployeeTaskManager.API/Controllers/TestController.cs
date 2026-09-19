81306a56d19f5ef0c8dd2699f17714b6f8cc4196
Mahesh Kakad <mahesh.kakad@gmail.com>
2026-09-19 14:48:52 +0530

    Commit 19 Sep 2026

---
+++ EmployeeTaskManagerSolution/EmployeeTaskManager.API/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeTaskManager.API.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
