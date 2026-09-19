516b233c944915d6c297c5c4b501abc1417ab3c7
Mahesh Kakad <mahesh.kakad@gmail.com>
2026-09-19 15:17:37 +0530

    Commit new changes003

---
+++ EmployeeTaskManagerSolution/EmployeeTaskManager.API/Controllers/MaincopyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeTaskManager.API.Controllers
+{
+    public class MaincopyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
