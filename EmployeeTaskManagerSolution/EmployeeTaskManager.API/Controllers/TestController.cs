5a3614ce44c3708655b3f715f60297c8eec37dad
Mahesh Kakad <mahesh.kakad@gmail.com>
2026-09-19 14:57:35 +0530

    Commit new changes002

--- EmployeeTaskManagerSolution/EmployeeTaskManager.API/Controllers/TestController.cs
+++ EmployeeTaskManagerSolution/EmployeeTaskManager.API/Controllers/TestController.cs
@@ -8,5 +8,10 @@ namespace EmployeeTaskManager.API.Controllers
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
